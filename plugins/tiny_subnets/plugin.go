2cfaeb3c081ff7efdc156c1a2e40e1973a9db9ee
ltsrad <alex+rad@longterm.io>
2022-04-17 15:39:42 -0700

    [tinysubnets] Disallow spaces

--- plugins/tiny_subnets/plugin.go
+++ plugins/tiny_subnets/plugin.go
@@ -46,7 +46,7 @@ func (p *PluginState) abstractDHCP(w http.ResponseWriter, r *http.Request) {
 					return
 	}
 
-	if req.Identifier == "" || (strings.TrimSpace(req.Identifier) != req.Identifier) {
+	if req.Identifier == "" || (strings.TrimSpace(req.Identifier) != req.Identifier) || req.Identifier.Contains(" ") {
 		http.Error(w, "Invalid Identifier", 400)
 		return
 	}
